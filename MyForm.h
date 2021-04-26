6f020633c973fda6da0ae2820edc880f5f926dd6
CaptainProninInSpace <CaptainProninInSpace@users.noreply.github.com>
2021-04-26 15:32:06 +0300

    Завершение работы над интерфейсом, начало работы над файлами

--- MyForm.h
+++ MyForm.h
@@ -8,6 +8,8 @@ namespace Kursovaya2 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	
+	using namespace System::IO;
 
 	/// <summary>
 	/// Сводка для MyForm
@@ -18,6 +20,10 @@ namespace Kursovaya2 {
 		MyForm(void)
 		{
 			InitializeComponent();
+
+			String^ fileName = "metadata.txt";
+
+			StreamWriter^ sw = gcnew StreamWriter(fileName);
 			//
 			//TODO: добавьте код конструктора
 			//
@@ -76,6 +82,15 @@ namespace Kursovaya2 {
 
 	private: System::Windows::Forms::Label^ label_GBADD_Nazvanie;
 	private: System::Windows::Forms::GroupBox^ GBPASS_groupBox_Passwords;
+	private: System::Windows::Forms::RichTextBox^ Output_Console;
+	private: System::Windows::Forms::ListBox^ listBox_PasswordsList;
+	private: System::Windows::Forms::GroupBox^ GBPASSINF_groupBox_PassInfo;
+	private: System::Windows::Forms::RichTextBox^ richTextBox_GBPASSINF_Primechanie;
+	private: System::Windows::Forms::Label^ label_GBPASSIF_Primechanie;
+	private: System::Windows::Forms::TextBox^ textBox_GBPASSINF_Site;
+	private: System::Windows::Forms::Label^ label_GBPASSINF_Site;
+	private: System::Windows::Forms::Button^ GBPASSINF_button_ShowHidePassword;
+	private: System::Windows::Forms::TextBox^ textBox_GBPASSINF_Password;
 
 
 
@@ -102,23 +117,34 @@ namespace Kursovaya2 {
 			this->checkBox_GBENT_RememberPassword = (gcnew System::Windows::Forms::CheckBox());
 			this->GBENT_groupBox_Vhod = (gcnew System::Windows::Forms::GroupBox());
 			this->GBREG_groupBox_Register = (gcnew System::Windows::Forms::GroupBox());
-			this->label_GBREG_CreateMasterPassword = (gcnew System::Windows::Forms::Label());
-			this->textBox_GBREG_CreateMasterPassword = (gcnew System::Windows::Forms::TextBox());
 			this->button_GBREG_CreateMasterPassword = (gcnew System::Windows::Forms::Button());
+			this->textBox_GBREG_CreateMasterPassword = (gcnew System::Windows::Forms::TextBox());
+			this->label_GBREG_CreateMasterPassword = (gcnew System::Windows::Forms::Label());
 			this->GBADD_groupBox_AddPassword = (gcnew System::Windows::Forms::GroupBox());
-			this->label_GBADD_Nazvanie = (gcnew System::Windows::Forms::Label());
-			this->textBox_GBADD_Nazvanie = (gcnew System::Windows::Forms::TextBox());
-			this->label_GBADD_Password = (gcnew System::Windows::Forms::Label());
-			this->textBox_GBADD_Password = (gcnew System::Windows::Forms::TextBox());
-			this->label_GBADD_Site = (gcnew System::Windows::Forms::Label());
-			this->textBox_GBADD_Site = (gcnew System::Windows::Forms::TextBox());
-			this->label_GBADD_Primechanie = (gcnew System::Windows::Forms::Label());
-			this->richTextBox_GBADD_Primechanie = (gcnew System::Windows::Forms::RichTextBox());
 			this->button_GBADD_AddPassword = (gcnew System::Windows::Forms::Button());
+			this->richTextBox_GBADD_Primechanie = (gcnew System::Windows::Forms::RichTextBox());
+			this->label_GBADD_Primechanie = (gcnew System::Windows::Forms::Label());
+			this->textBox_GBADD_Site = (gcnew System::Windows::Forms::TextBox());
+			this->label_GBADD_Site = (gcnew System::Windows::Forms::Label());
+			this->textBox_GBADD_Password = (gcnew System::Windows::Forms::TextBox());
+			this->label_GBADD_Password = (gcnew System::Windows::Forms::Label());
+			this->textBox_GBADD_Nazvanie = (gcnew System::Windows::Forms::TextBox());
+			this->label_GBADD_Nazvanie = (gcnew System::Windows::Forms::Label());
 			this->GBPASS_groupBox_Passwords = (gcnew System::Windows::Forms::GroupBox());
+			this->Output_Console = (gcnew System::Windows::Forms::RichTextBox());
+			this->GBPASSINF_groupBox_PassInfo = (gcnew System::Windows::Forms::GroupBox());
+			this->textBox_GBPASSINF_Password = (gcnew System::Windows::Forms::TextBox());
+			this->GBPASSINF_button_ShowHidePassword = (gcnew System::Windows::Forms::Button());
+			this->label_GBPASSINF_Site = (gcnew System::Windows::Forms::Label());
+			this->textBox_GBPASSINF_Site = (gcnew System::Windows::Forms::TextBox());
+			this->label_GBPASSIF_Primechanie = (gcnew System::Windows::Forms::Label());
+			this->richTextBox_GBPASSINF_Primechanie = (gcnew System::Windows::Forms::RichTextBox());
+			this->listBox_PasswordsList = (gcnew System::Windows::Forms::ListBox());
 			this->GBENT_groupBox_Vhod->SuspendLayout();
 			this->GBREG_groupBox_Register->SuspendLayout();
 			this->GBADD_groupBox_AddPassword->SuspendLayout();
+			this->GBPASS_groupBox_Passwords->SuspendLayout();
+			this->GBPASSINF_groupBox_PassInfo->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label_GBENT_Vhod
@@ -170,21 +196,22 @@ namespace Kursovaya2 {
 			this->GBREG_groupBox_Register->Name = L"GBREG_groupBox_Register";
 			this->GBREG_groupBox_Register->TabStop = false;
 			// 
-			// label_GBREG_CreateMasterPassword
+			// button_GBREG_CreateMasterPassword
 			// 
-			resources->ApplyResources(this->label_GBREG_CreateMasterPassword, L"label_GBREG_CreateMasterPassword");
-			this->label_GBREG_CreateMasterPassword->Name = L"label_GBREG_CreateMasterPassword";
+			resources->ApplyResources(this->button_GBREG_CreateMasterPassword, L"button_GBREG_CreateMasterPassword");
+			this->button_GBREG_CreateMasterPassword->Name = L"button_GBREG_CreateMasterPassword";
+			this->button_GBREG_CreateMasterPassword->UseVisualStyleBackColor = true;
+			this->button_GBREG_CreateMasterPassword->Click += gcnew System::EventHandler(this, &MyForm::button_GBREG_CreateMasterPassword_Click);
 			// 
 			// textBox_GBREG_CreateMasterPassword
 			// 
 			resources->ApplyResources(this->textBox_GBREG_CreateMasterPassword, L"textBox_GBREG_CreateMasterPassword");
 			this->textBox_GBREG_CreateMasterPassword->Name = L"textBox_GBREG_CreateMasterPassword";
 			// 
-			// button_GBREG_CreateMasterPassword
+			// label_GBREG_CreateMasterPassword
 			// 
-			resources->ApplyResources(this->button_GBREG_CreateMasterPassword, L"button_GBREG_CreateMasterPassword");
-			this->button_GBREG_CreateMasterPassword->Name = L"button_GBREG_CreateMasterPassword";
-			this->button_GBREG_CreateMasterPassword->UseVisualStyleBackColor = true;
+			resources->ApplyResources(this->label_GBREG_CreateMasterPassword, L"label_GBREG_CreateMasterPassword");
+			this->label_GBREG_CreateMasterPassword->Name = L"label_GBREG_CreateMasterPassword";
 			// 
 			// GBADD_groupBox_AddPassword
 			// 
@@ -201,62 +228,120 @@ namespace Kursovaya2 {
 			this->GBADD_groupBox_AddPassword->Name = L"GBADD_groupBox_AddPassword";
 			this->GBADD_groupBox_AddPassword->TabStop = false;
 			// 
-			// label_GBADD_Nazvanie
-			// 
-			resources->ApplyResources(this->label_GBADD_Nazvanie, L"label_GBADD_Nazvanie");
-			this->label_GBADD_Nazvanie->Name = L"label_GBADD_Nazvanie";
-			// 
-			// textBox_GBADD_Nazvanie
-			// 
-			resources->ApplyResources(this->textBox_GBADD_Nazvanie, L"textBox_GBADD_Nazvanie");
-			this->textBox_GBADD_Nazvanie->Name = L"textBox_GBADD_Nazvanie";
-			// 
-			// label_GBADD_Password
-			// 
-			resources->ApplyResources(this->label_GBADD_Password, L"label_GBADD_Password");
-			this->label_GBADD_Password->Name = L"label_GBADD_Password";
-			// 
-			// textBox_GBADD_Password
-			// 
-			resources->ApplyResources(this->textBox_GBADD_Password, L"textBox_GBADD_Password");
-			this->textBox_GBADD_Password->Name = L"textBox_GBADD_Password";
-			// 
-			// label_GBADD_Site
-			// 
-			resources->ApplyResources(this->label_GBADD_Site, L"label_GBADD_Site");
-			this->label_GBADD_Site->Name = L"label_GBADD_Site";
-			// 
-			// textBox_GBADD_Site
-			// 
-			resources->ApplyResources(this->textBox_GBADD_Site, L"textBox_GBADD_Site");
-			this->textBox_GBADD_Site->Name = L"textBox_GBADD_Site";
-			// 
-			// label_GBADD_Primechanie
-			// 
-			resources->ApplyResources(this->label_GBADD_Primechanie, L"label_GBADD_Primechanie");
-			this->label_GBADD_Primechanie->Name = L"label_GBADD_Primechanie";
-			// 
-			// richTextBox_GBADD_Primechanie
-			// 
-			resources->ApplyResources(this->richTextBox_GBADD_Primechanie, L"richTextBox_GBADD_Primechanie");
-			this->richTextBox_GBADD_Primechanie->Name = L"richTextBox_GBADD_Primechanie";
-			// 
 			// button_GBADD_AddPassword
 			// 
 			resources->ApplyResources(this->button_GBADD_AddPassword, L"button_GBADD_AddPassword");
 			this->button_GBADD_AddPassword->Name = L"button_GBADD_AddPassword";
 			this->button_GBADD_AddPassword->UseVisualStyleBackColor = true;
 			// 
+			// richTextBox_GBADD_Primechanie
+			// 
+			resources->ApplyResources(this->richTextBox_GBADD_Primechanie, L"richTextBox_GBADD_Primechanie");
+			this->richTextBox_GBADD_Primechanie->Name = L"richTextBox_GBADD_Primechanie";
+			// 
+			// label_GBADD_Primechanie
+			// 
+			resources->ApplyResources(this->label_GBADD_Primechanie, L"label_GBADD_Primechanie");
+			this->label_GBADD_Primechanie->Name = L"label_GBADD_Primechanie";
+			// 
+			// textBox_GBADD_Site
+			// 
+			resources->ApplyResources(this->textBox_GBADD_Site, L"textBox_GBADD_Site");
+			this->textBox_GBADD_Site->Name = L"textBox_GBADD_Site";
+			// 
+			// label_GBADD_Site
+			// 
+			resources->ApplyResources(this->label_GBADD_Site, L"label_GBADD_Site");
+			this->label_GBADD_Site->Name = L"label_GBADD_Site";
+			// 
+			// textBox_GBADD_Password
+			// 
+			resources->ApplyResources(this->textBox_GBADD_Password, L"textBox_GBADD_Password");
+			this->textBox_GBADD_Password->Name = L"textBox_GBADD_Password";
+			// 
+			// label_GBADD_Password
+			// 
+			resources->ApplyResources(this->label_GBADD_Password, L"label_GBADD_Password");
+			this->label_GBADD_Password->Name = L"label_GBADD_Password";
+			// 
+			// textBox_GBADD_Nazvanie
+			// 
+			resources->ApplyResources(this->textBox_GBADD_Nazvanie, L"textBox_GBADD_Nazvanie");
+			this->textBox_GBADD_Nazvanie->Name = L"textBox_GBADD_Nazvanie";
+			// 
+			// label_GBADD_Nazvanie
+			// 
+			resources->ApplyResources(this->label_GBADD_Nazvanie, L"label_GBADD_Nazvanie");
+			this->label_GBADD_Nazvanie->Name = L"label_GBADD_Nazvanie";
+			// 
 			// GBPASS_groupBox_Passwords
 			// 
+			this->GBPASS_groupBox_Passwords->Controls->Add(this->listBox_PasswordsList);
 			resources->ApplyResources(this->GBPASS_groupBox_Passwords, L"GBPASS_groupBox_Passwords");
 			this->GBPASS_groupBox_Passwords->Name = L"GBPASS_groupBox_Passwords";
 			this->GBPASS_groupBox_Passwords->TabStop = false;
+			// 
+			// Output_Console
+			// 
+			this->Output_Console->BackColor = System::Drawing::Color::White;
+			resources->ApplyResources(this->Output_Console, L"Output_Console");
+			this->Output_Console->Name = L"Output_Console";
+			// 
+			// GBPASSINF_groupBox_PassInfo
+			// 
+			this->GBPASSINF_groupBox_PassInfo->Controls->Add(this->richTextBox_GBPASSINF_Primechanie);
+			this->GBPASSINF_groupBox_PassInfo->Controls->Add(this->label_GBPASSIF_Primechanie);
+			this->GBPASSINF_groupBox_PassInfo->Controls->Add(this->textBox_GBPASSINF_Site);
+			this->GBPASSINF_groupBox_PassInfo->Controls->Add(this->label_GBPASSINF_Site);
+			this->GBPASSINF_groupBox_PassInfo->Controls->Add(this->GBPASSINF_button_ShowHidePassword);
+			this->GBPASSINF_groupBox_PassInfo->Controls->Add(this->textBox_GBPASSINF_Password);
+			resources->ApplyResources(this->GBPASSINF_groupBox_PassInfo, L"GBPASSINF_groupBox_PassInfo");
+			this->GBPASSINF_groupBox_PassInfo->Name = L"GBPASSINF_groupBox_PassInfo";
+			this->GBPASSINF_groupBox_PassInfo->TabStop = false;
+			// 
+			// textBox_GBPASSINF_Password
+			// 
+			resources->ApplyResources(this->textBox_GBPASSINF_Password, L"textBox_GBPASSINF_Password");
+			this->textBox_GBPASSINF_Password->Name = L"textBox_GBPASSINF_Password";
+			// 
+			// GBPASSINF_button_ShowHidePassword
+			// 
+			resources->ApplyResources(this->GBPASSINF_button_ShowHidePassword, L"GBPASSINF_button_ShowHidePassword");
+			this->GBPASSINF_button_ShowHidePassword->Name = L"GBPASSINF_button_ShowHidePassword";
+			this->GBPASSINF_button_ShowHidePassword->UseVisualStyleBackColor = true;
+			// 
+			// label_GBPASSINF_Site
+			// 
+			resources->ApplyResources(this->label_GBPASSINF_Site, L"label_GBPASSINF_Site");
+			this->label_GBPASSINF_Site->Name = L"label_GBPASSINF_Site";
+			// 
+			// textBox_GBPASSINF_Site
+			// 
+			resources->ApplyResources(this->textBox_GBPASSINF_Site, L"textBox_GBPASSINF_Site");
+			this->textBox_GBPASSINF_Site->Name = L"textBox_GBPASSINF_Site";
+			// 
+			// label_GBPASSIF_Primechanie
+			// 
+			resources->ApplyResources(this->label_GBPASSIF_Primechanie, L"label_GBPASSIF_Primechanie");
+			this->label_GBPASSIF_Primechanie->Name = L"label_GBPASSIF_Primechanie";
+			// 
+			// richTextBox_GBPASSINF_Primechanie
+			// 
+			resources->ApplyResources(this->richTextBox_GBPASSINF_Primechanie, L"richTextBox_GBPASSINF_Primechanie");
+			this->richTextBox_GBPASSINF_Primechanie->Name = L"richTextBox_GBPASSINF_Primechanie";
+			// 
+			// listBox_PasswordsList
+			// 
+			this->listBox_PasswordsList->FormattingEnabled = true;
+			resources->ApplyResources(this->listBox_PasswordsList, L"listBox_PasswordsList");
+			this->listBox_PasswordsList->Name = L"listBox_PasswordsList";
 			// 
 			// MyForm
 			// 
 			resources->ApplyResources(this, L"$this");
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->Controls->Add(this->GBPASSINF_groupBox_PassInfo);
+			this->Controls->Add(this->Output_Console);
 			this->Controls->Add(this->GBPASS_groupBox_Passwords);
 			this->Controls->Add(this->GBADD_groupBox_AddPassword);
 			this->Controls->Add(this->GBREG_groupBox_Register);
@@ -269,11 +354,23 @@ namespace Kursovaya2 {
 			this->GBREG_groupBox_Register->PerformLayout();
 			this->GBADD_groupBox_AddPassword->ResumeLayout(false);
 			this->GBADD_groupBox_AddPassword->PerformLayout();
+			this->GBPASS_groupBox_Passwords->ResumeLayout(false);
+			this->GBPASSINF_groupBox_PassInfo->ResumeLayout(false);
+			this->GBPASSINF_groupBox_PassInfo->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	};
+
+	
+
+	private: System::Void button_GBREG_CreateMasterPassword_Click(System::Object^ sender, System::EventArgs^ e) {
+		textBox_GBREG_CreateMasterPassword->Text = "";
+		GBREG_groupBox_Register->Visible = false;
+
+	}
+
+		   
+};
 }
 
-int a = 0;
